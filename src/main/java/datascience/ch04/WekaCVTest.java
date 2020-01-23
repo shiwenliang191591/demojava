d0d1ca7fe361e094239641c3292d712a737c1cc1
shiwenliang <shiet@126.com>
2020-01-23 19:31:06 +0800

    commit

--- src/main/java/datascience/ch04/WekaCVTest.java
+++ src/main/java/datascience/ch04/WekaCVTest.java
@@ -1,4 +1,62 @@
 package datascience.ch04;
 
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.util.Random;
+
 public class WekaCVTest {
+    Instances iris = null;
+    NaiveBayes nb;
+
+    public void loadArff(String arffInput) {
+        DataSource source = null;
+        try {
+            source = new DataSource(arffInput);
+            iris = source.getDataSet();
+            if(iris.classIndex() == -1)
+                iris.setClassIndex(iris.numAttributes() - 1);
+        } catch (Exception e1) {
+
+        }
+    }
+
+    public void generateModel() {
+        nb = new NaiveBayes();
+        try {
+            nb.buildClassifier(iris);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void saveModel(String modelPath) {
+        try {
+            SerializationHelper.write(modelPath, nb);
+        } catch(Exception e) {
+
+        }
+    }
+
+    public void crossValidate() {
+        Evaluation eval = null;
+        try {
+            eval = new Evaluation(iris);
+            eval.crossValidateModel(nb, iris, 10, new Random(1));
+            System.out.println(eval.toSummaryString());
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        WekaCVTest test = new WekaCVTest();
+        test.loadArff("/Users/shiwenliang/Downloads/tools/weka-3-6-15/data/iris.arff");
+        test.generateModel();
+        test.saveModel("/Users/shiwenliang/Downloads/project/dataanalysis/demojava/data/nb.model");
+        test.crossValidate();
+    }
 }
--- src/main/java/datascience/ch04/WekaTrainTest.java
+++ src/main/java/datascience/ch04/WekaTrainTest.java
@@ -1,4 +1,87 @@
 package datascience.ch04;
 
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+
+import java.io.*;
+
 public class WekaTrainTest {
+    NaiveBayes nb;
+    Instances train, test, labeled;
+
+    public void loadModel(String modelPath) {
+        try {
+            nb = (NaiveBayes) SerializationHelper.read(modelPath);
+        } catch(Exception e) {
+
+        }
+    }
+
+    public void loadDatasets(String training, String testing) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(training));
+            train = new Instances(reader);
+            train.setClassIndex(train.numAttributes() - 1);
+        } catch(Exception e) {
+
+        }
+
+        try {
+            reader = new BufferedReader(new FileReader(testing));
+            test = new Instances(reader);
+            test.setClassIndex(test.numAttributes() - 1);
+        } catch(Exception e) {
+
+        }
+
+        try {
+            reader.close();
+        } catch(Exception e) {
+
+        }
+    }
+
+    public void classify() {
+        try {
+            nb.buildClassifier(train);
+        } catch(Exception e) {
+
+        }
+
+        labeled = new Instances(test);
+
+        for(int i = 0; i < test.numInstances(); i ++) {
+            double clsLabel;
+            try {
+                clsLabel = nb.classifyInstance(test.instance(i));
+                labeled.instance(i).setClassValue(clsLabel);
+                double[] predict = nb.distributionForInstance(test.instance(i));
+                double predictPro = predict[1];
+                System.out.println(predictPro);
+            } catch(Exception e) {
+
+            }
+        }
+    }
+
+    public void writeArff(String outarff) {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(outarff));
+            writer.write(labeled.toString());
+            writer.close();
+        } catch(Exception e) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        WekaTrainTest test = new WekaTrainTest();
+        test.loadModel("/Users/shiwenliang/Downloads/project/dataanalysis/demojava/data/nb.model");
+        test.loadDatasets("/Users/shiwenliang/Downloads/project/dataanalysis/demojava/data/iris.arff","/Users/shiwenliang/Downloads/project/dataanalysis/demojava/data/iris-test.arff");
+        test.classify();
+        test.writeArff("/Users/shiwenliang/Downloads/project/dataanalysis/demojava/data/trainarff.arff");
+    }
 }
